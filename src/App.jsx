1c510187514afbcb6a6d4a647bb63095e38f0dd7
shriv-cpu <vshri029@gmail.com>
2025-08-29 15:57:35 +0530

    Initial commit: Contact Us project with React + Vite + Tailwind

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { useState } from 'react'
+
+import './App.css'
+import ContactForm from "./assets/ContactForm";
+
+function App() {
+  return (
+    <div className="flex items-center justify-center min-h-screen bg-gray-100">
+      <ContactForm />
+    </div>
+  );
+}
+
+export default App;
+
